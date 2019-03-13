0004dfebd12cce16d5b1b87dcbcb08ef8db2129d
P3 <668292@my.sbunified.org>
2019-03-13 12:45:44 -0700

    'helee'

--- webapp.py
+++ webapp.py
@@ -24,52 +24,70 @@ mail=Mail(app)
 def Page1():
     return render_template('Page1.html')
 
+    
 @app.route('/page2')
 def Page2():
     return render_template('Page2.html')
     
-
-
-
+    
+@app.route('/page3')
+def Page3():
+    return render_template('Page3.html')
+    
+    
 @app.route('/next1',methods=["POST","GET"])
 def rendernext1():
     #messg = request.form['data']
     return render_template('Page2.html')
 
+
 @app.route('/next2',methods=["POST","GET"])
 def rendernext2():
-
-    messg = "'" + str(request.form['data']) + "'"
-    msg = Message('User Dats', sender = 'codingbot0@gmail.com', recipients = ['codingbot0@gmail.com'])
-    msg.attach("data.csv", "data/csv" , messg )
-
-    mail.send(msg)
+    
+    session["message1"] = request.form['data']
+    
     return render_template('Page3.html' , sent="msg1 sent")
     
+
 @app.route('/next3',methods=["POST","GET"])
 def rendernext3():
 
-    messg = "'" + str(request.form['data']) + "'"
-    msg = Message('User Dats', sender = 'codingbot0@gmail.com', recipients = ['codingbot0@gmail.com'])
-    msg.attach("data.csv", "data/csv" , messg )
-
-    mail.send(msg)
+    session["message2"] = request.form['data']
+    
+    
     return render_template('Page4.html' , sent="msg2 sent")
     
+
 @app.route('/next4',methods=["POST","GET"])
 def rendernext4():
+    
+    session["message3"] = request.form['data']
+    
+    return render_template('Page5.html' , sent="msg3 sent")
+    
 
-    messg = "'" + str(request.form['data']) + "'"
-    msg = Message('User Dats', sender = 'codingbot0@gmail.com', recipients = ['codingbot0@gmail.com'])
+@app.route('/next5',methods=["POST","GET"])
+def rendernext5():
+   
+   session["message4"] = request.form['data']
+
+    
+   return render_template('Page6.html' , sent="msg4 sent")
+    
+    
+@app.route('/next6',methods=["POST","GET"])
+def rendernext6():
+    
+    session["message5"] = request.form['data'] + ',' + request.form['data1']
+     
+    messg = session['message1'] + ',' + session['message2'] + ',' + session['message3'] + ',' + session['message4'] + ',' + session['message5'] 
+    msg = Message('User Dats', sender = 'codingbot@gmail.com', recipients = ['codingbot0@gmail.com'])
     msg.attach("data.csv", "data/csv" , messg )
 
     mail.send(msg)
-    return render_template('Page5.html' , sent="msg3 sent")
+    
+    return render_template('Page7.html' , sent="msg5 sent")
 
-
-@app.route('/home',methods=["POST","GET"])
-def renderhome():
-    return render_template('Page1.html')
 
 if __name__ == '__main__':
     os.system("echo json(array) > file")
