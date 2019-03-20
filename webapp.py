94668799f5583f7f3734eeee563be7741381b57f
P3 <668292@my.sbunified.org>
2019-03-20 12:47:37 -0700

    sscscsc

--- webapp.py
+++ webapp.py
@@ -6,6 +6,8 @@ from time import localtime, strftime
 import csv
 import pprint
 import os
+import random
+
 
 app = Flask(__name__)
 
@@ -25,6 +27,11 @@ mail=Mail(app)
 def Page1():
     
     print(strftime("%a %d %b %Y %H:%M:%S", localtime()))
+    for i in range(20):
+        print(random.randint(10000,99999))
+    
+
+    
     return render_template('Page1.html')
     
 
@@ -81,16 +88,29 @@ def rendernext5():
     
 @app.route('/next6',methods=["POST","GET"])
 def rendernext6():
-    
+    math = ['murphy','phelps','ruissner']
+    english = ['slemp','light','nye']
+    history = ['tormey','mendoza','jackson']
+    s = ['cadenasso','poster','quinn']
+    for i in range(1):
+        session["math"] = random.choice(math)
+        session["english"] = random.choice(english)
+        session["history"] = random.choice(history)
+        session["science"] = random.choice(s)
+    for i in range(20):
+        session["code1"]=(random.randint(10000,99999))
+        session["code2"]=(random.randint(10000,99999))
+        session["code3"]=(random.randint(10000,99999))
+        session["code4"]=(random.randint(10000,99999))
     session["message5"] = request.form['data'] + ',' + request.form['data1'] + ',' + request.form['data2'] + ',' + strftime("%a %d %b %Y %H:%M:%S", localtime())
     
-    messg = session['message1'] + ',' + session['message2'] + ',' + session['message3'] + ',' + session['message4'] + ',' + session['message5'] 
+    messg = 'Math' + ',' + 'English' + ',' + 'History' + ',' + 'Science' + ',' + 'Weighted GPA' + ',' + "Unweighted GPA" + ',' + "Student ID" + ',' + "Time Sent" + "\n" + session['message1'] + ',' + session['message2'] + ',' + session['message3'] + ',' + session['message4'] + ',' + session['message5']
     msg = Message('User Dats', sender = 'codingbot@gmail.com', recipients = ['codingbot0@gmail.com'])
-    msg.attach("data.csv", "data/csv" , messg )
+    msg.attach("data.csv", "data/csv" , messg)
 
     mail.send(msg)
     
-    return render_template('Page7.html' , msg1=session["message1"], msg2= session["message2"],  msg3= session["message3"],  msg4= session["message4"],  time=strftime("%a, %d %b %Y %H:%M:%S ",localtime()))
+    return render_template('Page7.html' , math = session["math"], english = session["english"], history = session["history"], science = session["science"], code1 = session["code1"] , code2 = session["code2"] , code3 = session["code3"] , code4 = session["code4"] ,msg1=session["message1"], msg2= session["message2"],  msg3= session["message3"],  msg4= session["message4"],  time=strftime("%a, %d %b %Y %H:%M:%S ",localtime()))
     
 
 
